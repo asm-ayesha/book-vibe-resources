f2577511a84e2911250d4bc6309a69e35601fc4c
asm-ayesha <ayesha219333@gmail.com>
2026-04-09 17:54:27 +0600

    load books data and show all the books

---
+++ src/components/Ui/BookCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { CiStar } from 'react-icons/ci';
+const BookCard = ({ book }) => {
+    return (
+
+        <div className="card bg-base-100 shadow-sm">
+            <figure className='p-6 '>
+                <img
+                    src={book.image}
+                    alt={book.bookName}
+                    className='rounded-lg h-[260px]' />
+            </figure>
+            <div className="card-body">
+                <div className='flex items-center gap-2'>
+                    {
+                        book.tags.map((tag, ind) => <div key={ind} className="badge text-green-500 bg-green-50 font-bold">{tag}</div>)
+                    }
+                </div>
+
+                <h2 className="card-title">
+                    <h2 className='font-bold text-2xl'>{book.bookName}</h2>
+
+                </h2>
+                <p className='text-lg font-semibold'>By: {book.author}</p>
+
+                <div className="card-actions justify-between border-t border-dashed border-gray-300 pt-4 text-xl">
+                    <div className="font-semibold">{book.category}</div>
+                    <div className="flex gap-2 items-center">{book.rating} <CiStar></CiStar>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default BookCard;
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../components/Banner/Banner';
+import AllBooks from '../../components/Banner/AllBooks';
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
+            <AllBooks></AllBooks>
         </div>
     );
 };
